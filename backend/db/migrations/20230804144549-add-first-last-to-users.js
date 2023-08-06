305eee3fff7001c9ec8a1bc431e7464cfc86fc93
will <woburns191919>
2023-08-05 21:29:29 -0500

    latest

--- backend/db/migrations/20230804144549-add-first-last-to-users.js
+++ backend/db/migrations/20230804144549-add-first-last-to-users.js
@@ -1,34 +1,31 @@
-'use strict';
+"use strict";
 
-
-let options = {}
+let options = {};
 options.tableName = "Users";
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;
 }
 
-
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn(options, 'firstName', {
+    await queryInterface.addColumn(options, "firstName", {
       type: Sequelize.STRING(30),
       allowNull: false,
-    })
-    await queryInterface.addColumn(options, 'lastName', {
+    });
+    await queryInterface.addColumn(options, "lastName", {
       type: Sequelize.STRING(30),
       allowNull: false,
-    })
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
@@ -36,7 +33,7 @@ module.exports = {
      * await queryInterface.dropTable('users');
      */
     options.tableName = "Users";
-    await queryInterface.removeColumn(options, 'firstName')
-    await queryInterface.removeColumn(options, 'lastName')
-  }
+    await queryInterface.removeColumn(options, "firstName");
+    await queryInterface.removeColumn(options, "lastName");
+  },
 };
