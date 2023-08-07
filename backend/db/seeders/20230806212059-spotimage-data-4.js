5656ecb3de2513e460452ec459619da7460f7290
will <woburns191919>
2023-08-07 12:36:20 -0500

    old seeders

---
+++ backend/db/seeders/20230806212059-spotimage-data-4.js
@@ -0,0 +1,47 @@
+'use strict';
+const { SpotImage } = require('../models')
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await SpotImage.bulkCreate([
+    {
+      spotId: 4,
+      url: "https://goo.gl/maps/M9THTWLbaNCJ81Gk7",
+      preview: true
+    }
+   ], { validate: true })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     options.tableName = "SpotImages"
+     return queryInterface.bulkDelete(
+       options,
+       {
+         spotId: 4
+       },
+       {}
+     );
+   },
+ };
