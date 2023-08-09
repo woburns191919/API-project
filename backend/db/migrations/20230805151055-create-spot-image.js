631b2de63887700e497c6e83dc69e9d52bb874cc
will <woburns191919>
2023-08-09 16:37:19 -0500

    added reset

--- backend/db/migrations/20230805151055-create-spot-image.js
+++ backend/db/migrations/20230805151055-create-spot-image.js
@@ -43,6 +43,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "SpotImages"
-    await queryInterface.dropTable('SpotImages');
+    await queryInterface.dropTable(options);
   }
 };
