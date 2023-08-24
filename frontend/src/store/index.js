357175f897248045e0f7f25b1b2970d02473c360
will <woburns191919>
2023-08-24 07:28:33 -0500

    phase1 thunk

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
-const rootReducer = combineReducers({
-  // add reducer functions here
-});
 
 let enhancer;
+
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+});
+
 
 if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
