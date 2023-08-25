846335bebac7a9e9c8fd8022225095084781a943
will <woburns191919>
2023-08-25 18:19:43 -0500

    new database

--- backend/db/seeders/20230807175418-spot-data.js
+++ backend/db/seeders/20230807175418-spot-data.js
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
-const { Spot } = require('../models')
-
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;
 }
 
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -20,86 +19,94 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-   await Spot.bulkCreate([
-    {
-      ownerId: 2,
-      address: "140 Washington St",
-      city: "New York",
-      state: "New York",
-      country: "United States of America",
-      lat: 40.71020,
-      lng: -74.01402,
-      name: "Club Quarters Hotel",
-      description: "Great place to stay in the city",
-      price: 200
-    },
-    {
-      ownerId: 3,
-      address: "200 Congress Pl",
-      city: "Cape May",
-      state: "New Jersey",
-      country: "United States of America",
-      lat: 38.93204,
-      lng: -74.92321,
-      name: "Congress Hall",
-      description: "Classy place in Cape May",
-      price: 300
-    },
-    {
-      ownerId: 4,
-      address: "400 E Capitol Ave",
-      city: "Little Rock",
-      state: "Arkansas",
-      country: "United States of America",
-      lat: 34.74513,
-      lng: -92.26761,
-      name: "Hilton Garden Inn",
-      description: "Nice spot by the river",
-      price: 200
-    },
-    {
-      ownerId: 3,
-      address: "300 Kansas st",
-      city: "Baileyville",
-      state: "Kansas",
-      country: "United States of America",
-      lat: 38.74513,
-      lng: -90.26761,
-      name: "Zome on the Range",
-      description: "Nice zome",
-      price: 400
-    },
-    {
-      ownerId: 4,
-      address: "20 Montana St",
-      city: "Bozeman",
-      state: "Montana",
-      country: "United States of America",
-      lat: 38.74513,
-      lng: -91.26761,
-      name: "Silo",
-      description: "Nice silo",
-      price: 200
-    }
-   ], { validate: true }
-   );
+     */
+    await User.bulkCreate(
+      [
+        {
+          email: "Oblivion@gmail.com",
+          username: "Oblivion",
+          hashedPassword: bcrypt.hashSync("mrsquishy"),
+          firstName: "David",
+          lastName: "Fosterwallace",
+        },
+        {
+          email: "biggie@gmail.com",
+          username: "biggie",
+          hashedPassword: bcrypt.hashSync("bigpappa"),
+          firstName: "Christopher",
+          lastName: "Wallace",
+        },
+        {
+          email: "junot@gmail.com",
+          username: "Junot",
+          hashedPassword: bcrypt.hashSync("asteroid"),
+          firstName: "Junot",
+          lastName: "Diaz",
+        },
+        {
+          email: "emergency@hotmail.com",
+          username: "emergency",
+          hashedPassword: bcrypt.hashSync("jesusson"),
+          firstName: "Denis",
+          lastName: "Johnson",
+        },
+        // {
+        //   email: "nas@hotmail.com",
+        //   username: "nas",
+        //   hashedPassword: bcrypt.hashSync("queensbridge"),
+        //   firstName: "Nasir",
+        //   lastName: "Jones",
+        // },
+        // {
+        //   email: "whereareyougoing@gmail.com",
+        //   username: "whereareyougoing",
+        //   hashedPassword: bcrypt.hashSync("prolific"),
+        //   firstName: "Joyce",
+        //   lastName: "Caroloates",
+        // },
+        // {
+        //   email: "unsafejobs@gmail.com",
+        //   username: "unsafejobs",
+        //   hashedPassword: bcrypt.hashSync("gnomes"),
+        //   firstName: "Alissa",
+        //   lastName: "Nutting",
+        // },
+        // {
+        //   email: "arrangedmarriages@gmail.com",
+        //   username: "arrangedmarriages",
+        //   hashedPassword: bcrypt.hashSync("before"),
+        //   firstName: "Chitra",
+        //   lastName: "Divakaruni",
+        // },
+        // {
+        //   email: "therethere@gmail.com",
+        //   username: "therethere",
+        //   hashedPassword: bcrypt.hashSync("channelorange"),
+        //   firstName: "Tommy",
+        //   lastName: "Orange",
+        // },
+      ],
+      { validate: true }
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-     options.tableName = "Spots"
-     return queryInterface.bulkDelete(
-       options,
-       {
-         ownerId: [1, 2, 3]
-       },
-       {}
-     );
-   },
- };
+    options.tableName = "Users";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        firstName: {
+          [Op.in]: ["David", "Christopher", "Junot", "Denis"]
+        },
+      },
+      {}
+    );
+  },
+};
