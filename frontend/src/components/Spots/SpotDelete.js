dc3f8875df0515a4ab35911beacd0945a65bfe28
will <woburns191919>
2023-09-01 11:17:55 -0500

    newdbd

--- frontend/src/components/Spots/SpotDelete.js
+++ frontend/src/components/Spots/SpotDelete.js
@@ -3,22 +3,24 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import { Link, NavLink, useParams, useHistory } from "react-router-dom";
-import { useModal } from '../../context/Modal';
+import { closeModal, useModal } from '../../context/Modal';
 
-import { thunkSpotDelete, thunkGetSpotDetails } from "../../store/spots";
+import { thunkSpotDelete, thunkGetSpotDetails, thunkGetCurrentSpots } from "../../store/spots";
 // import { closeModal } from "../context/Modal"
 
 const SpotDelete = ({ spotId }) => {
   // const { spotId } = useParams();
-
+  const { closeModal } = useModal()
   // console.log('use params', useParams())
-
   const dispatch = useDispatch();
-
+  const handleDelete = async () => {
+    await dispatch(thunkSpotDelete(spotId))
+    await dispatch(thunkGetCurrentSpots())
+    closeModal();
+  }
 //   useEffect(() => {
 //     dispatch(thunkSpotDelete(spotId)).then(data => console.log('deleted', data))
 // }, [dispatch])
-
 console.log('is this running?')
 
 if (!spotId) return null;
@@ -27,10 +29,12 @@ return (
   <h5>Confirm Delete</h5>
   <h4>Are you sure you want to remove this spot from the listings?</h4>
   <button
-  onClick={() => dispatch(thunkSpotDelete(spotId))}
+  onClick={handleDelete}
   >Yes(Delete Spot)
   </button>
-  <button>No (Keep Spot)</button>
+  <button
+  onClick={closeModal}
+  >No (Keep Spot)</button>
   </>
 )
 }
