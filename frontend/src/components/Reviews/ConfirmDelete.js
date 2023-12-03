21108f15c97d701605bd46fc64303acaed04ff6d
will <woburns191919>
2023-12-03 11:54:24 -0600

    inline modal styling

--- frontend/src/components/Reviews/ConfirmDelete.js
+++ frontend/src/components/Reviews/ConfirmDelete.js
@@ -4,8 +4,8 @@ import { thunkReviewDelete } from "../../store/reviews";
 
 import { thunkGetReviewsBySpotId, thunkGetSpotDetails } from "../../store/spots";
 
-import "./Reviews.css"
-import "../Spots/SpotDelete.css"
+// import "./Reviews.css"
+// import "../Spots/SpotDelete.css"
 // import "../Spots/SpotDelete.css"
 
 const ConfirmDelete = ( { reviewId, spotId } ) => {
@@ -18,6 +18,50 @@ const ConfirmDelete = ( { reviewId, spotId } ) => {
 
   // console.log('spotId', spotId)
 
+  const modalContainerStyle = {
+    backgroundColor: 'white',
+    padding: '20px',
+    borderRadius: '12px',
+    boxShadow: '0 10px 40px rgba(0, 0, 0, 0.1)',
+    textAlign: 'center'
+  };
+
+  const headingStyle = {
+    color: '#484848',
+    marginBottom: '15px'
+  };
+
+  const paragraphStyle = {
+    color: '#484848',
+    marginBottom: '20px'
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#FF385C',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    margin: '5px'
+  };
+
+  const noButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: 'darkgray'
+  };
+
+  const buttonContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '10px'
+  };
+
+
+
+
+
   const handleDelete = async () => {
     await dispatch(thunkReviewDelete(reviewId, spotId));
     await dispatch(thunkGetReviewsBySpotId(spotId));
@@ -26,23 +70,27 @@ const ConfirmDelete = ( { reviewId, spotId } ) => {
 
   if (!reviewId) return null;
 
+
   return (
     <>
-    <div id="delete-spot-confirm-modal">
-      <h5>Confirm Delete</h5>
-      <p id="delete-sure">Are you sure you want to remove this spot from the listings?</p>
-      <div id="yes-button-review-delete-button">
-      <button
-      value={reviewId}
-      onClick={handleDelete}
-      >Yes(Delete Review)
-      </button>
-      </div>
-      <div id="no-button-review-delete-button">
-      <button
-      onClick={closeModal}
-      >No (Keep Review)</button>
-      </div>
+      <div id="delete-spot-confirm-modal" style={modalContainerStyle}>
+        <h5 style={headingStyle}>Confirm Delete</h5>
+        <p id="delete-sure" style={paragraphStyle}>Are you sure you want to remove this review?</p>
+        <div style={buttonContainerStyle}>
+          <button
+            style={buttonStyle}
+            value={reviewId}
+            onClick={handleDelete}
+          >
+            Yes (Delete Review)
+          </button>
+          <button
+            style={noButtonStyle}
+            onClick={closeModal}
+          >
+            No (Keep Review)
+          </button>
+        </div>
       </div>
     </>
   );
