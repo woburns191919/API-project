146192f0fcfd0fc0a1896b7c7b54d89526d9563a
will <woburns191919>
2023-12-04 10:10:35 -0600

    update reserve

--- frontend/src/components/Spots/BookingManage.js
+++ frontend/src/components/Spots/BookingManage.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetUserBookings } from "../../store/bookings";
+import "./SpotsManage.css";
+import "./GetAllSpots.css";
 
 const BookingManage = () => {
   const dispatch = useDispatch();
@@ -12,22 +14,27 @@ const BookingManage = () => {
 
   // console.log('bookings from store', userBookings?.Bookings[0].id)
 
-
   return (
-    <main className="booking-manage-wrapper">
-      <h3>Your Bookings</h3>
-      {userBookings?.Bookings && userBookings.Bookings.length > 0 ? (
-        userBookings.Bookings.map((booking, index) => (
-          <div key={index} className="booking-item">
-            <div>Booking ID: {booking.id}</div>
-            <div>Spot ID: {booking.spotId}</div>
-            <div>Start Date: {booking.startDate}</div>
-            <div>End Date: {booking.endDate}</div>
-          </div>
-        ))
-      ) : (
-        <p>No bookings found.</p>
-      )}
+    <main className="outer-wrapper">
+      <header className="manage-create-current-spots">
+        <h3>Manage Your Bookings</h3>
+      </header>
+      <div className="photo-container">
+        {userBookings?.Bookings && userBookings.Bookings.length > 0 ? (
+          userBookings.Bookings.map((booking, index) => (
+            <div key={index} className="inner-Container">
+              <div className="booking-info">
+                <div>Booking ID: {booking.id}</div>
+                <div>Spot ID: {booking.spotId}</div>
+                <div>Start Date: {booking.startDate}</div>
+                <div>End Date: {booking.endDate}</div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No bookings found.</p>
+        )}
+      </div>
     </main>
   );
 };
