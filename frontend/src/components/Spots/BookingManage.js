2d0e1a7460275febe78deadcd9f93575e1042335
will <woburns191919>
2023-12-06 07:41:50 -0600

    backend booking fix

--- frontend/src/components/Spots/BookingManage.js
+++ frontend/src/components/Spots/BookingManage.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetUserBookings } from "../../store/bookings";
+import { thunkGetSpotDetails } from "../../store/spots";
 import { Link, NavLink } from "react-router-dom";
 import OpenModalButton from "../OpenModalButton";
 import BookingDelete from "./BookingDelete";
 import "./SpotsManage.css";
 import "./GetAllSpots.css";
 
+
 const BookingManage = () => {
+  // console.log('spot id?', spotId)
   const dispatch = useDispatch();
   const userBookings = useSelector((state) => state.bookings.userBookings);
+
 
   useEffect(() => {
     dispatch(thunkGetUserBookings());
@@ -22,38 +26,44 @@ const BookingManage = () => {
       </header>
       <div className="photo-container">
         {userBookings?.Bookings && userBookings.Bookings.length > 0 ? (
-          userBookings.Bookings.map((booking, i) => (
-            <div key={i} className="inner-Container">
-              <Link to={`/spots/${booking.Spot.id}`}>
-                <img src={booking.Spot.previewImage} alt={`Preview of Spot ${booking.Spot.id}`} className="spot-image" />
-                <div className="info">
-                  <div className="left-info">
-                    <div className="city-state">
-                      {booking.Spot.city}, {booking.Spot.state}
+          userBookings.Bookings.map((booking, i) => {
+            const previewImage = booking.Spot.previewImage;
+
+            return (
+              <div key={i} className="inner-Container">
+                <Link to={`/spots/${booking.Spot.id}`}>
+                  {previewImage && (
+                    <img src={previewImage} alt={`Preview of Spot ${booking.Spot.id}`} className="spot-image" />
+                  )}
+                  <div className="info">
+                    <div className="left-info">
+                      <div className="city-state">
+                        {booking.Spot.city}, {booking.Spot.state}
+                      </div>
+                      <div className="star-info">
+                        <i className="fa fa-star"></i>
+                        {booking.Spot.avgRating > 0 ? booking.Spot.avgRating.toFixed(2) : 'new'}
+                      </div>
                     </div>
-                    <div className="star-info">
-                      <i className="fa fa-star"></i>
-                      {booking.Spot.avgRating > 0 ? booking.Spot.avgRating.toFixed(2) : 'new'}
+                    <div className="right-info">
+                      <b>${booking.Spot.price}</b> night
+                    </div>
+                    <div className="booking-dates">
+                      <p>Start Date: {booking.startDate}</p>
+                      <p>End Date: {booking.endDate}</p>
                     </div>
                   </div>
-                  <div className="right-info">
-                    <b>${booking.Spot.price}</b> night
-                  </div>
-                  <div className="booking-dates">
-                    <p>Start Date: {booking.startDate}</p>
-                    <p>End Date: {booking.endDate}</p>
-                  </div>
+                </Link>
+                <div className="booking-actions">
+                  <NavLink to={`/bookings/edit/${booking.id}`} className="booking-action-button">Update</NavLink>
+                  <OpenModalButton
+                    buttonText="Cancel"
+                    modalComponent={<BookingDelete bookingId={booking.id} />}
+                  />
                 </div>
-              </Link>
-              <div className="booking-actions">
-                <NavLink to={`/bookings/edit/${booking.id}`} className="update-button">Update</NavLink>
-                <OpenModalButton
-                  buttonText="Cancel"
-                  modalComponent={<BookingDelete bookingId={booking.id} />}
-                />
               </div>
-            </div>
-          ))
+            );
+          })
         ) : (
           <p>No bookings found.</p>
         )}
