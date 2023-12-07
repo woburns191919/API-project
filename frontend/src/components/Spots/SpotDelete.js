173965d0338825868e3a25dd7190b60576e1fbd4
will <woburns191919>
2023-12-07 09:48:21 -0600

    style update

--- frontend/src/components/Spots/SpotDelete.js
+++ frontend/src/components/Spots/SpotDelete.js
@@ -1,52 +1,82 @@
-
-
 import { useDispatch } from "react-redux";
-
-
 import { useModal } from '../../context/Modal';
-
 import { thunkSpotDelete, thunkGetCurrentSpots } from "../../store/spots";
-// import { closeModal } from "../context/Modal"
-import "./SpotDelete.css";
-
 
 const SpotDelete = ({ spotId }) => {
-  // const { spotId } = useParams();
-  const { closeModal } = useModal()
-  // console.log('use params', useParams())
+  const { closeModal } = useModal();
   const dispatch = useDispatch();
+
   const handleDelete = async () => {
-    await dispatch(thunkSpotDelete(spotId))
-    await dispatch(thunkGetCurrentSpots())
+    await dispatch(thunkSpotDelete(spotId));
+    await dispatch(thunkGetCurrentSpots());
     closeModal();
-  }
-//   useEffect(() => {
-//     dispatch(thunkSpotDelete(spotId)).then(data => console.log('deleted', data))
-// }, [dispatch])
+  };
 
+  if (!spotId) return null;
 
-if (!spotId) return null;
-return (
-  <>
-  {/* <div className="confirmdelete"> */}
-  <h5>Confirm Delete</h5>
-  {/* </div> */}
-  <h4 class="sure">Are you sure you want to remove this spot from the listings?</h4>
-  <div className="buttondiv">
-  <button
-  id="yesbutton"
-  onClick={handleDelete}
-  >Yes(Delete Spot)
-  </button>
-  <button
-  className="nobutton"
-  onClick={closeModal}
-  >No (Keep Spot)</button>
-  </div>
-  </>
-)
-}
+  // Styles
+  const modalContainerStyle = {
+    backgroundColor: 'white',
+    padding: '20px',
+    borderRadius: '12px',
+    boxShadow: '0 10px 40px rgba(0, 0, 0, 0.1)',
+    textAlign: 'center'
+  };
 
-// frontend/src/context/Modal.js
+  const headingStyle = {
+    color: '#484848',
+    marginBottom: '15px'
+  };
+
+  const paragraphStyle = {
+    color: '#484848',
+    marginBottom: '20px'
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#FF385C',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    margin: '5px'
+  };
+
+  const noButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: 'darkgray'
+  };
+
+  const buttonContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '10px'
+  };
+
+  return (
+    <>
+      <div id="delete-spot-confirm-modal" style={modalContainerStyle}>
+        <h5 style={headingStyle}>Confirm Delete</h5>
+        <p id="delete-sure" style={paragraphStyle}>Are you sure you want to remove this spot from the listings?</p>
+        <div style={buttonContainerStyle}>
+          <button
+            style={buttonStyle}
+            onClick={handleDelete}
+          >
+            Yes (Delete Spot)
+          </button>
+          <button
+            style={noButtonStyle}
+            onClick={closeModal}
+          >
+            No (Keep Spot)
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SpotDelete;
