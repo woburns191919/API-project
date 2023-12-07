c5e2f1adb0b9108ad5362799ed23296412a053c8
will <woburns191919>
2023-12-07 16:59:51 -0600

    styles update spot manage

---
+++ frontend/src/components/Spots/SpotCardManage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import OpenModalButton from "../OpenModalButton";
+import SpotDelete from "./SpotDelete";
+import "./SpotCardManage.css"; // A separate CSS file for this component
+
+const SpotCardManage = ({ spot }) => {
+  return (
+    <div className="spot-card-manage">
+      <Link to={`/spots/${spot.id}`} className="spot-link-manage">
+        <img className="spot-image-manage" alt={spot.name} src={spot.previewImage} />
+        <div className="spot-info-manage">
+          <div className="spot-location-manage">{spot.city}, {spot.state}</div>
+          <div className="spot-rating-manage">
+            <i className="fa fa-star"></i>
+            {spot.avgRating > 0 ? spot.avgRating.toFixed(2) : "new"}
+          </div>
+          <div className="spot-price-manage"><b>${spot.price}</b> per night</div>
+        </div>
+      </Link>
+      <div className="spot-actions-manage">
+        <NavLink to={`/spots/edit/${spot.id}`} className="booking-action-button">Update</NavLink>
+        <OpenModalButton
+          buttonText="Delete"
+          modalComponent={<SpotDelete spotId={spot.id} />}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SpotCardManage;
