facf2d8b0305c34ce30be1eb448c29bd557fab51
will <woburns191919>
2023-12-09 09:27:59 -0600

    form styles w margin

--- frontend/src/components/Spots/SpotForm.js
+++ frontend/src/components/Spots/SpotForm.js
@@ -132,7 +132,7 @@ const SpotForm = () => {
   return (
     <main className="form-wrapper">
       {formError && <div className="error-message">{formError}</div>}
-      <form className="spot-form" onSubmit={handleSubmit}>
+      <form className="spot-form" onSubmit={handleSubmit} style={{marginTop: '70%'}}>
         <h3>Create a new Spot</h3>
         <div className="form-top-info">
           <h4>Where's your place located?</h4>
