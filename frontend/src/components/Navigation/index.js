6d0a40a256102eaafa5b205bacd68b45c6777532
will <woburns191919>
2024-01-12 11:31:24 -0600

    navstyles

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,7 +6,6 @@ import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 
-
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
@@ -46,27 +45,42 @@ function Navigation({ isLoaded }) {
             <h6>Slick Spots</h6>
           </NavLink>
         </div>
+        <div className="nav-middle">
+          <div className="nav-item">
+            <p className="anywhere">Anywhere</p>
+          </div>
+          <div className="nav-item">
+            <p className="anyweek">Any week</p>
+          </div>
+          <div className="nav-item add-guests">
+            <p>Add guests</p>
+            <div className="icon-box">
+              <i className="fa fa-search search-icon"></i>
+            </div>
+          </div>
+        </div>
         <div className="upper-right-nav-wrap">
-        <div className="create-spot-in-manage-spots">
-          {sessionUser && (
-             <NavLink to="/spots/new" className="rent-home-link">Rent Your Home</NavLink>
-          )}
-        </div>
-        <div className="globe">
-        <i class="fa fa-globe"></i>
-        </div>
+          <div className="create-spot-in-manage-spots">
+            {sessionUser && (
+              <NavLink to="/spots/new" className="rent-home-link">
+                Rent Your Home
+              </NavLink>
+            )}
+          </div>
+          <div className="globe">
+            <i class="fa fa-globe"></i>
+          </div>
 
-        <div className="header-right">
-          <div className="list-icon">
-            <i className="fa fa-list"></i>
-          </div>
-          <div className="sign-up">
-            {isLoaded && <ProfileButton user={sessionUser} />}
-          </div>
+          <div className="header-right">
+            <div className="list-icon">
+              <i className="fa fa-list"></i>
+            </div>
+            <div className="sign-up">
+              {isLoaded && <ProfileButton user={sessionUser} />}
+            </div>
           </div>
         </div>
       </header>
-      <hr></hr>
     </>
   );
 }
